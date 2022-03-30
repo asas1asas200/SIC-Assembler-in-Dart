311717775f3cfc0de64a111dcc8386c43c626528
asas1asas200 <asas1asas200@gmail.com>
2022-03-30 23:12:04 +0800

    add assembler and make it parse tokens

---
+++ assembler.dart
@@ -0,0 +1,8 @@
+class Assembler {
+  final String filename;
+  final List<List<String>> instructions;
+
+  Assembler(
+      {required this.filename,
+      required List<List<String>> this.instructions}) {}
+}
--- main.dart
+++ main.dart
@@ -1,13 +1,28 @@
 import 'dart:io';
 
+import 'assembler.dart';
+
+Future<Assembler> initParser(String filename) async {
+  var inputs = await File(filename).readAsLines();
+  return Assembler(
+      filename: filename.substring(0, filename.indexOf('.')),
+      instructions: inputs
+          .where((x) => x.isNotEmpty && x[0] != '.')
+          .map(
+              (x) => x.split(RegExp(r'\s')).where((x) => x.isNotEmpty).toList())
+          .toList());
+}
+
 void main(List<String> args) async {
+  var parser = null;
+
   try {
-    var filename = args[0];
-    var file = File(args[0]);
-    var contents = await file.readAsLines();
+    parser = await initParser(args[0]);
   } catch (e) {
     print('Raise an exception when read file.');
     print(e);
     return;
   }
+
+  print(parser.instructions);
 }
